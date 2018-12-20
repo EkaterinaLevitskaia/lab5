b0bdb0eee9d3d0227ebdc063e60b6c1eb621bf8e
Omikronikus <johny9763@gmail.com>
2018-12-20 18:13:56 +0600

    Потоки первая попытка

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        First ft = new First();
+        First st = new First();
+        while (true){
+
+            ft.run();
+            st.run();
+        }
+    }
+}
